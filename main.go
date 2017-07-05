d5e73fd44fae9a788610946380ef8cd63b32e66b
Jack Burdick <jackbburdick@gmail.com>
2017-07-04 21:31:47 -0400

    Include main file with strategy plan

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+// ----------- Mosaic Image
+// Read image
+
+// Determine avg color
+
+// Break into color "buckets"
+
+// Break each bucket into Light - Med - Dark (or range, based on defined value)
+
+// ---------- Target Image
+// Read target image
+
+// Downsample target image to X,Y
+
+// Determine average pix color
+
+// Break colors into "buckets" [determined by `mosaic buckets`]
+
+// Light - Med - Dark color for each bucket (or range, based on defined value)
+
+// ------------ Image Creation
+// Map Mosaic images to target image
+
+// Create new image w/each mosaic image mapped to the target image as a 'pixel'
+
+func main() {
+	// Read mosaic images to see what values we have to work with.
+
+	// Read in target image to see how we have to map
+
+	// Map mosaic images to target image values
+
+	// Create final image
+
+	// Profit
+
+	fmt.Println("yipee")
+}
